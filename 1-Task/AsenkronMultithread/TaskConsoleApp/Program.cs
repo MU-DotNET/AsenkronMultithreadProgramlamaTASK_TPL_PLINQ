b1ef8705502df108e83cf3212011f008e8f7bf27
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2024-07-16 20:14:39 +0300

    6. Task Method: ContinueWith

---
+++ 1-Task/AsenkronMultithread/TaskConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿
+#region ContunieWith
+//Console.WriteLine("Hello, World!");
+
+//Task? myTask = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith((data) =>
+//{
+//	Console.WriteLine("data uzunluk :" + data.Result.Length);
+//});
+
+//Console.WriteLine("Arada yapılacak işler");
+
+//await myTask; 
+#endregion
+#region WithoutContinueWith
+//Console.WriteLine("Hello, World!");
+
+//Task<string>? myTask = new HttpClient().GetStringAsync("https://www.google.com");
+
+//Console.WriteLine("Arada yapılacak işler");
+
+//string? data = await myTask;
+
+//Console.WriteLine("data uzunluk :" + data.Length);
+
+#endregion
+#region ContinueWithLambdaFunction
+
+Console.WriteLine("Hello, World!");
+
+var myTask = new HttpClient().GetStringAsync("https://www.google.com")
+    .ContinueWith(calis);
+
+Console.WriteLine("Arada yapılacak işler");
+
+await myTask;
+
+
+void calis(Task<string> data)
+{
+    Console.WriteLine("data uzunluk: " + data.Result.Length);
+}
+
+#endregion
