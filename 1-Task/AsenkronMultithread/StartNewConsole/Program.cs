73e42618de9bb28235ddd98c8cb07666308c9f49
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2024-07-20 13:42:05 +0300

    13. Task Method: StartNew()

---
+++ 1-Task/AsenkronMultithread/StartNewConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Task? myTask = Task.Factory.StartNew((obj) =>
+{
+    Console.WriteLine("myTask çalıştı");
+    Status? status = obj as Status;
+    status.ThreadId = Thread.CurrentThread.ManagedThreadId;
+}, new Status() { Date = DateTime.Now });
+
+await myTask;
+Status s = myTask.AsyncState as Status;
+Console.WriteLine(s.Date);
+Console.WriteLine(s.ThreadId);
+
+class Status
+{
+    public int ThreadId { get; set; }
+    public DateTime Date { get; set; }
+
+}
