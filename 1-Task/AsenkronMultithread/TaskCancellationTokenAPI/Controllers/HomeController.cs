66162057f2e374f2e3edc365e76f00d236942ee1
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2024-07-21 17:45:00 +0300

    16. CancellationToken -2

---
+++ 1-Task/AsenkronMultithread/TaskCancellationTokenAPI/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskCancellationTokenAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController(ILogger<HomeController> _logger) : ControllerBase
+    {
+        public async Task<IActionResult> GetContentAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("İstek Başladı");
+            try
+            {
+                await Task.Delay(5000, cancellationToken);
+                Task<string>? myTask = new HttpClient().GetStringAsync("https://www.google.com", cancellationToken);
+                string? data = await myTask;
+                _logger.LogInformation("İstek Bitti");
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("İstek iptal edildi : " + ex.Message);
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
