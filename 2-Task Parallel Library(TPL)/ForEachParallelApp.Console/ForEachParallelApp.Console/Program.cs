54498105648528f538fafc65e3e777a7a3abb2b6
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2024-07-28 11:48:38 +0300

    25. Parallel.ForEach -2

--- 2-Task Parallel Library(TPL)/ForEachParallelApp.Console/ForEachParallelApp.Console/Program.cs
+++ 2-Task Parallel Library(TPL)/ForEachParallelApp.Console/ForEachParallelApp.Console/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System.Diagnostics;
 using System.Drawing;
 
-ParallelForeach();
-Foreach();
+ParallelForeachFileBytes();
 
 void ParallelForeach()
 {
@@ -32,7 +31,7 @@ void Foreach()
     string picturesPath = "D:\\Github\\AsenkronMultithreadProgramlamaTASK_TPL_PLINQ\\2-Task Parallel Library(TPL)\\Pictures";
     string[]? files = Directory.GetFiles(picturesPath);
 
-    files.ToList().ForEach((file) => 
+    files.ToList().ForEach((file) =>
     {
         Console.WriteLine($"Thread No: {Thread.CurrentThread.ManagedThreadId}");
         Image image = new Bitmap(file);
@@ -43,4 +42,30 @@ void Foreach()
 
     stopwatch.Stop();
     Console.WriteLine("Foreach İşlem tamamlandı : " + stopwatch.ElapsedMilliseconds);
+}
+
+void ParallelForeachFileBytes()
+{
+    long fileBytes = 0;
+    Stopwatch stopwatch = new();
+    stopwatch.Start();
+
+    string picturesPath = "D:\\Github\\AsenkronMultithreadProgramlamaTASK_TPL_PLINQ\\2-Task Parallel Library(TPL)\\Pictures";
+    string[]? files = Directory.GetFiles(picturesPath);
+
+    Parallel.ForEach(files, (item) =>
+    {
+        Console.WriteLine("Thread No: " + Thread.CurrentThread.ManagedThreadId);
+        FileInfo fileInfo = new(item);
+        Console.WriteLine($"{fileInfo.Name} Length : {fileInfo.Length}");
+
+        //fileBytes += fileInfo.Length;
+
+        Interlocked.Add(ref fileBytes, fileInfo.Length);
+        Interlocked.Exchange(ref fileBytes, 300);
+    });
+    Console.WriteLine("Total bytes : " + fileBytes);
+    Console.WriteLine("İşlem Bitti : " + stopwatch.ElapsedMilliseconds);
+
+
 }
