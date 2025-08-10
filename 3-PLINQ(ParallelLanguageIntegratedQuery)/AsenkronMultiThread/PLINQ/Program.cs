88f1d212b9157ef8999d2bbb88edfc0659a4f996
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-08-10 22:53:42 +0300

    feat: implement basic exception handling and logging

--- 3-PLINQ(ParallelLanguageIntegratedQuery)/AsenkronMultiThread/PLINQ/Program.cs
+++ 3-PLINQ(ParallelLanguageIntegratedQuery)/AsenkronMultiThread/PLINQ/Program.cs
@@ -3,6 +3,48 @@ using PLINQ.Entities;
 
 MyDbContext context = new MyDbContext();
 
+
+#region Exception Handle
+
+var products = context.Products.Take(100).ToArray();
+
+products[3].Name = "##";
+
+products[5].Name = "##";
+
+var query = products.AsParallel().Where(isControl);
+
+try
+{
+query.ForAll(x => Console.WriteLine(x.Name));
+}
+catch (AggregateException ex)
+{
+    ex.InnerExceptions.ToList().ForEach(x =>
+    {
+        if (x is IndexOutOfRangeException)
+        {
+        Console.WriteLine("Hata: array sınırları dışına çıkıldı.");
+        }
+    });
+}
+
+bool isControl(Product p)
+{
+    try
+    {
+        return p.Name[2]=='a';
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Dizi Sınırları aşışdı");
+        return false;
+    }
+}
+
+#endregion
+
+#region AsOrdered
 // var product = (from p in context.Products.AsParallel()
 //     where p.ListPrice > 10M
 //     select p).Take(10);
@@ -25,9 +67,12 @@ MyDbContext context = new MyDbContext();
 
 // context.Products.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).ForAll(p => WriteLog(p));
 
-context.Products.AsParallel().AsOrdered().Where(p => p.ListPrice > 10M).ToList().ForEach(p => Console.WriteLine($"{p.Name} -  {p.ListPrice}"));
+// context.Products.AsParallel().AsOrdered().Where(p => p.ListPrice > 10M).ToList().ForEach(p => Console.WriteLine($"{p.Name} -  {p.ListPrice}"));
+//
+// void WriteLog(Product product)
+// {
+//     Console.WriteLine(product.Name + " log'a kaydedildi.");
+// }
 
-void WriteLog(Product product)
-{
-    Console.WriteLine(product.Name + " log'a kaydedildi.");
-}
+
+#endregion
