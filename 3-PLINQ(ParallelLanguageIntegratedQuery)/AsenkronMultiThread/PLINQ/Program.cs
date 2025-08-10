ebb536ea9f80cdc60b61f71c28cf948189191436
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-08-10 22:35:11 +0300

    feat: implement PLINQ parallel processing on Entity objects for improved concurrency

--- 3-PLINQ(ParallelLanguageIntegratedQuery)/AsenkronMultiThread/PLINQ/Program.cs
+++ 3-PLINQ(ParallelLanguageIntegratedQuery)/AsenkronMultiThread/PLINQ/Program.cs
@@ -1,4 +1,30 @@
 ﻿using PLINQ.Context;
+using PLINQ.Entities;
 
 MyDbContext context = new MyDbContext();
-context.Products.Take(10).ToList().ForEach(x => Console.WriteLine(x.Name));
+
+// var product = (from p in context.Products.AsParallel()
+//     where p.ListPrice > 10M
+//     select p).Take(10);
+
+// var product = (from p in context.Products
+//         where p.ListPrice > 10M
+//             select p).Take(10);
+//
+// product.AsParallel().ForAll(x => Console.WriteLine($"{x.Name} -> {x.ListPrice}"));
+    
+// product.ToList().ForEach(x => Console.WriteLine(x.Name));
+
+// product.ForAll(x => Console.WriteLine(x));
+
+// var product = (from p in context.Products.AsParallel()
+//     where p.ListPrice > 10M
+//     select p).Take(10);
+
+context.Products.AsParallel().ForAll(p => WriteLog(p));
+
+
+void WriteLog(Product product)
+{
+    Console.WriteLine(product.Name + " log'a kaydedildi.");
+}
