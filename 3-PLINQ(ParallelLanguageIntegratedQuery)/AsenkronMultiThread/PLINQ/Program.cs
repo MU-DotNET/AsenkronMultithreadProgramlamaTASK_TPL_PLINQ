9aaba760a4df2e724760d2c0ea505d8a29627603
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-08-10 19:32:05 +0300

    feat: Use AsParallel() in LINQ query

---
+++ 3-PLINQ(ParallelLanguageIntegratedQuery)/AsenkronMultiThread/PLINQ/Program.cs
@@ -0,0 +1,13 @@
+﻿
+bool Islem(int x)
+{
+    return x % 2 == 0;
+}
+
+var array = Enumerable.Range(1,100).ToList();
+
+array.Where(Islem).ToList().ForEach(x => Console.WriteLine(x));
+
+var newArray = array.AsParallel().Where(Islem);
+
+newArray.ToList().ForEach(x => Console.WriteLine(x));
