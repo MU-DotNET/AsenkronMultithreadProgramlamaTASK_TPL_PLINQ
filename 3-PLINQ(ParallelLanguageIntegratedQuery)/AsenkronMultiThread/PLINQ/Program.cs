1593ccbc8c8b53a1238936da263a00de2d149447
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-08-10 22:40:10 +0300

    feat: implement WithExecutionMode to customize PLINQ execution mode

--- 3-PLINQ(ParallelLanguageIntegratedQuery)/AsenkronMultiThread/PLINQ/Program.cs
+++ 3-PLINQ(ParallelLanguageIntegratedQuery)/AsenkronMultiThread/PLINQ/Program.cs
@@ -21,8 +21,9 @@ MyDbContext context = new MyDbContext();
 //     where p.ListPrice > 10M
 //     select p).Take(10);
 
-context.Products.AsParallel().WithDegreeOfParallelism(2).ForAll(p => WriteLog(p));
+//context.Products.AsParallel().WithDegreeOfParallelism(2).ForAll(p => WriteLog(p));
 
+context.Products.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).ForAll(p => WriteLog(p));
 
 void WriteLog(Product product)
 {
